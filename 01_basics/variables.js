2355e1563ebe819b5147f87099ee134a2a00d9f9
ekjot2345-collab <ekjot2345@gmail.com>
2026-02-15 16:54:21 +0530

    <add variable basics>

---
+++ 01_basics/variables.js
@@ -0,0 +1,19 @@
+const accountId=144553
+let accountEmail="ekjot994@gmail.com"
+var accountPassword="123455"
+accountCity="jaipur"
+let accountState;
+
+
+// accountId=2//not allowed
+accountEmail="nvifjijf@gmail.com"
+accountPassword="155652"
+accountCity="delhi"
+
+console.log(accountId)
+
+console.table([accountId,accountEmail,accountPassword,accountState])
+// value will be undefined if no value is initiated to the variable
+/*
+prefer not to use var due to some scope reasons
+ */
