e16a76b74de4606dc255adfd1b78325b7b843fbf
ekjot2345-collab <ekjot2345@gmail.com>
2026-02-18 12:42:57 +0530

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,33 @@
+ "user strict"; //treat all js code as newer version 
+
+//  alert(3+3) we are using node js  , not browser 
+
+console.log(3
+    
++
+ 3)   //code readability should be high
+
+console.log("hitesh")
+
+
+
+
+// let name="hitesh"
+// let age="3"
+// let isLoggedIn=false
+// let state= null
+
+//number=>2 to power 54
+//bigint
+//string=>""
+//boolean=>true/false
+//null=>standalone value
+//undefined=>
+//symbol=>unique
+
+
+//object
+
+
+console.log(typeof undefined)
+console.log(typeof null)
