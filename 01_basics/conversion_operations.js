94240ebebf0a3cf5580ca39deb487fa2f02e513b
ekjot2345-collab <ekjot2345@gmail.com>
2026-02-19 17:28:38 +0530

    update

--- 01_basics/conversion_operations.js
+++ 01_basics/conversion_operations.js
@@ -5,7 +5,7 @@
 // // let score=undefined
 // console.log(valueInNumber); //NaN
 // let score=true//1
-let score="hitesh"
+//let score="hitesh"
 
 
 // console.log(typeof score);
@@ -20,10 +20,77 @@ let score="hitesh"
 // console.log(booleanIsloggedIn)
 //true when hitesh
 
-let someNumber=33
-let stringNumber=String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let someNumber=33
+// let stringNumber=String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+                                                     //OPERATIONS//
+// let value=3
+// let negValue=-3
+// console.log(negValue)
+// console.log(3+3)
+// console.log(3-3)
+// console.log(4-9+7-8)
+// console.log(5*7)
+// console.log(45%8)
+// console.log(45/9)
+// console.log(45/9)
 
 
 
+// let str1="hitesh"
+// let str2="manan"
+// let str3=str1+str2
+// console.log(str3)
+
+// console.log("1"+"2")
+// console.log("1"+2)
+// console.log("2"+1)
+
+
+// console.log("1"+2+2)//if you have written string first (like here you have wriiten 1 ) 
+// // then all will be treated as a strings , 
+// // nothing willl add
+
+
+// console.log(1+2+"2")//if string at last then normal (32 as here )
+
+// console.log(true); true
+// console.log(+true); 1
+
+
+
+// console.log(true)    error
+let num1,num2,num3 
+num1=num2=num3=2+2
+
+let gameCounter=100
+// gameCounter++;
+//output100
+// gameCounter--;
+//output99
+// console.log(gameCounter)
+
+
+                                 //postfix increment// variable++
+// let x = 3;
+// const y = x++;
+// // x is 4; y is 3 
+//value to y is assigned before performing operation on x
+
+// let x2 = 3n;
+// const y2 = x2++;
+// // x2 is 4n; y2 is 3n
+
+                                    //prefix increment//++variable
+
+let x=3
+const y=++x
+console.log(y,x)
+//x amd y  both are 4 (:::value to y is assigned after performing operation on x)
+let x2 = 3n;
+const y2 = ++x2;
+console.log(y2,x2)
+// x2 is 4n; y2 is 3n
--- 01_basics/tempCodeRunnerFile.js
+++ 01_basics/tempCodeRunnerFile.js
@@ -1,7 +1,1 @@
-//  "user strict"; //treat all js code as newer version 
-
-//  alert(3+3) we are using node js  , not browser 
-
-console.log(3+3)
-
-console.log("hitesh")
+gameCounter++;
