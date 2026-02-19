c94193df50595518942764dd289d2f368b44864f
ekjot2345-collab <ekjot2345@gmail.com>
2026-02-19 16:14:20 +0530

    updated

--- 01_basics/conversion_operations.js
+++ 01_basics/conversion_operations.js
@@ -1,8 +1,29 @@
-let score='66abc'
+// let score='66abc'
+// console.log(valueInNumber); //Nan
+// // let score= null  
+// console.log(valueInNumber); //0
+// // let score=undefined
+// console.log(valueInNumber); //NaN
+// let score=true//1
+let score="hitesh"
 
-console.log(typeof score);
-console.log(typeof(score))
 
-let valueInNumber=Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);  
+// console.log(typeof score);
+// console.log(typeof(score))
+
+// let valueInNumber=Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+ 
+// let isLoggedIn="hitesh"
+// let booleanIsloggedIn=Boolean(isLoggedIn)
+// console.log(booleanIsloggedIn)
+//true when hitesh
+
+let someNumber=33
+let stringNumber=String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
