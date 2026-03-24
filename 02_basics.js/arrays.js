9664fb9a3bb6fec720d6d1db97003f7208f91ad9
ekjot2345-collab <ekjot2345@gmail.com>
2026-03-24 09:40:49 +0530

    save

---
+++ 02_basics.js/arrays.js
@@ -0,0 +1,39 @@
+//ARRAY
+
+const myArry=[1,2,4,89,58]
+const myFavSingers=["Arijit Singh","Shreya Ghoshal","DILJIT DOSANJH","Neha Kakkar","Atif Aslam"]
+
+//PROTOTYPE JUST REMBER IN ARRAY PROPERTY 
+
+const myArr2=new Array(1,2,3,4,50)
+console.log(myArry[3])
+console.log(myFavSingers[2])
+
+
+
+//ARRAY METTHODS
+
+// myArr2.push(60)
+// myArr2.push(70)
+// myArr2.pop()
+    // myArr2.unshift(0)  //need to shift all the values to right and add 0 at the start of the array
+    // myArr2.shift()
+
+
+// console.log(myArr2);
+// console.log(myArr2.length)
+
+
+//  console.log(myArr2.indexOf(8))//-1
+
+
+// console.log(myArr2.includes(4))
+
+
+const newArr=myArr2.join()
+console.log(newArr)
+console.log(typeof newArr);
+
+
+//slice,spice
+
