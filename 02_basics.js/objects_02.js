15064ec389bd85b10484ca761554abf4598a6b09
ekjot2345-collab <ekjot2345@gmail.com>
2026-05-29 17:09:19 +0530

    save

--- 02_basics.js/objects_02.js
+++ 02_basics.js/objects_02.js
@@ -52,3 +52,29 @@ console.log(Object.entries(tinderUser))
 
 
 console.log(tinderUser.hasOwnProperty("name"))//true
+
+
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "hitesh"
+}
+
+// course.courseInstructor
+
+const {courseInstructor: instructor} = course
+
+// console.log(courseInstructor);
+console.log(instructor);
+
+// {
+//     "name": "hitesh",
+//     "coursename": "js in hindi",
+//     "price": "free"
+// }
+
+[
+    {},
+    {},
+    {}
+]
